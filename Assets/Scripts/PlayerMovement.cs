15852f97066ff8e0161c3bad5c24c08a6cc458ad
Teo-CD <teocd.perso@outlook.fr>
2024-11-30 17:24:03 +0000

    PlayerMovement: Don't control the transform directly
    Controlling the transform directly is easy but leads to
    glitchy movement when colliding and does not integrate well
    with the rest of the physics system.
    
    Change the previous transform-based movement to a speed-based
    one. Make sure that the character stops quite quickly with a
    damping.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Transform _transform;
+    private Rigidbody2D _body;
+    private Vector2 _dampVelocity = Vector2.zero;
 
     [SerializeField] private float speed = 5f;
     [SerializeField] private SerialHandler serialHandler;
@@ -12,14 +13,22 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _transform = GetComponentInParent<Transform>();
+        _body = GetComponentInParent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"),0);
-        _transform.position += direction.normalized * speed * Time.deltaTime;
+        Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if ( direction.magnitude == 0)
+        {
+            _body.velocity = Vector2.SmoothDamp(_body.velocity, Vector2.zero, ref _dampVelocity, 0.05f);
+        }
+        else
+        {
+            _dampVelocity = Vector2.zero;
+            _body.velocity = Vector2.ClampMagnitude(_body.velocity + direction.normalized * speed, speed);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
